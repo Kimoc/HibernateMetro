03223353bce603923f452fe9bfb0154dbf2e639a
Kimoc <kimocfarmer@gmail.com>
2019-12-05 09:06:14 +0100

    ya inserta

---
+++ src/vista/VentanaInsertarLineaEstacion.java
@@ -0,0 +1,130 @@
+package vista;
+
+import hibernateutil.*;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import hibernateutil.HibernateUtil;
+import modelo.TEstaciones;
+import modelo.TLineaEstacion;
+import modelo.TLineaEstacionId;
+import modelo.TLineas;
+
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class VentanaInsertarLineaEstacion extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField tfNumLinea;
+	private JTextField tfNumEstacion;
+	private JTextField tfNumOrden;
+	private JLabel lblNewLabel_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaInsertarLineaEstacion frame = new VentanaInsertarLineaEstacion();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaInsertarLineaEstacion() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		tfNumLinea = new JTextField();
+		tfNumLinea.setBounds(212, 38, 124, 19);
+		contentPane.add(tfNumLinea);
+		tfNumLinea.setColumns(10);
+		
+		tfNumEstacion = new JTextField();
+		tfNumEstacion.setBounds(212, 72, 124, 19);
+		contentPane.add(tfNumEstacion);
+		tfNumEstacion.setColumns(10);
+		
+		tfNumOrden = new JTextField();
+		tfNumOrden.setBounds(212, 98, 124, 19);
+		contentPane.add(tfNumOrden);
+		tfNumOrden.setColumns(10);
+		
+		//Metodo para insertar t_linea_estacion
+		JButton btInsertar = new JButton("Insertar");
+		btInsertar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+				Session session = sessionFactory.openSession();
+				Transaction transaction = session.beginTransaction();
+				if(Integer.valueOf(tfNumLinea.getText()) > 0 && Integer.valueOf(tfNumEstacion.getText()) > 0 
+						&& Integer.valueOf(tfNumOrden.getText()) > 0  ) {
+					
+					TLineas linea = session.load(TLineas.class, Integer.valueOf(tfNumLinea.getText()));
+					TEstaciones estaciones = session.load(TEstaciones.class, Integer.valueOf(tfNumEstacion.getText()));
+					
+					TLineaEstacionId idLineaEstacion = new TLineaEstacionId(linea.getCodLinea(), estaciones.getCodEstacion());
+					TLineaEstacion lineaEstacion = new TLineaEstacion(idLineaEstacion, null, null, Integer.valueOf(tfNumOrden.getText()));
+					try {
+						session.save(lineaEstacion);
+						transaction.commit();
+						JOptionPane.showMessageDialog(null, "Insertada");
+					} catch (Exception a) {
+						a.printStackTrace();
+						JOptionPane.showMessageDialog(null, "Error");
+					}
+				} else {
+				}
+				
+				
+			}
+		});
+		btInsertar.setBounds(222, 168, 114, 25);
+		contentPane.add(btInsertar);
+		
+		JLabel lblNewLabel = new JLabel("Numero de Linea");
+		lblNewLabel.setBounds(37, 40, 144, 15);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNumero = new JLabel("Numero de Estación");
+		lblNumero.setBounds(37, 67, 157, 15);
+		contentPane.add(lblNumero);
+		
+		JLabel lblNumeroOrden = new JLabel("Numero Orden");
+		lblNumeroOrden.setBounds(37, 100, 124, 15);
+		contentPane.add(lblNumeroOrden);
+		
+		lblNewLabel_1 = new JLabel("Insertar t_linea_estacion");
+		lblNewLabel_1.setBackground(Color.BLUE);
+		lblNewLabel_1.setBounds(123, 12, 288, 15);
+		contentPane.add(lblNewLabel_1);
+	}
+}
---
+++ src/vista/VentanaPrincipal.java
@@ -0,0 +1,58 @@
+package vista;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class VentanaPrincipal extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaPrincipal frame = new VentanaPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaPrincipal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		//Abrir ventana para isnertar linea/Estacion
+		JButton btnInsertarNuevaLineaestacion = new JButton("Insertar nueva Linea/Estacion");
+		btnInsertarNuevaLineaestacion.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			   VentanaInsertarLineaEstacion ventainsert= new VentanaInsertarLineaEstacion();
+			   ventainsert.setVisible(true);
+			   ventainsert.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+				
+			}
+		});
+		btnInsertarNuevaLineaestacion.setBounds(189, 29, 239, 32);
+		contentPane.add(btnInsertarNuevaLineaestacion);
+	}
+
+}
